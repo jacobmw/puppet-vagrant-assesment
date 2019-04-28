31c152b357e6e9c7664a8976ce219e9c8a8deb67
Kevin Mathew <kemathew@cisco.com>
2019-04-28 00:36:37 -0700

    [CAE-24] changed port to 8080 and added new line to hello world

--- app/go-webapp.go
+++ app/go-webapp.go
@@ -9,10 +9,10 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	h, _ := os.Hostname()
 
-	fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
+	fmt.Fprintf(w, "Hi there, I'm served from %s!\n", h)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8484", nil)
+	http.ListenAndServe(":8080", nil)
 }
